ccf8cda102d5009a7ea35c98cb17a4665218ff4b
wmo <wmo>
2024-10-17 23:47:21 +0900

    Add CORS config

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -4,6 +4,18 @@ from datetime import datetime
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",  # Frontend server URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class Post(BaseModel):
     id: int
     title: str
