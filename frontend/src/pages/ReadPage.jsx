2da0955991b16deefd31ba9486bac902d9cbd911
LSJ <1007ckddjs12@gmail.com>
2024-10-25 16:51:09 +0900

    fix date format

--- frontend/src/pages/ReadPage.jsx
+++ frontend/src/pages/ReadPage.jsx
@@ -9,6 +9,8 @@ import MyViewer from "../components/MyViewer";
 
 const postURL = "http://127.0.0.1:8000/api/posts/detail/";
 
+const Category = "Category";
+
 const ReadPage = () => {
   const { postId } = useParams(); // URL에서 postID 가져오기
   const [post, setPost] = useState(null);
@@ -34,21 +36,24 @@ const ReadPage = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
+  const dateForm = (date) => {
+    const options = { year: "numeric", month: "long", day: "2-digit" };
+    return new Date(date).toLocaleDateString("ko-KR", options);
+  };
+
+  const createDate = dateForm(post.created_at);
+  const updateDate = dateForm(post.created_at);
+
   return (
     <ReadPageContainer>
       <PageHeader title={post?.title || "Post Title"} />
-      {post && (
-        <>
-          <InfoContainer>
-            <PostDate>
-              {new Date(post.created_at).toLocaleDateString()}
-            </PostDate>
-          </InfoContainer>
-          <ViewerContainer>
-            <MyViewer Content={post.content} />
-          </ViewerContainer>
-        </>
-      )}
+      <InfoContainer>
+        <PostCategory>{Category}</PostCategory>
+        <PostDate>{`${createDate}(${updateDate})`}</PostDate>
+      </InfoContainer>
+      <ViewerContainer >
+        <MyViewer Content={post.content} />
+      </ViewerContainer>
     </ReadPageContainer>
   );
 };
@@ -63,12 +68,18 @@ const ReadPageContainer = styled.div`
 
 const InfoContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   margin-bottom: 10px;
 `;
 
-const PostDate = styled.div`
-  font-size: 14px;
+const PostCategory = styled.h5`
+  margin-left: 10px;
+  margin-right: 20px;
+  color: ${({ theme }) => theme.bgText};
+`;
+
+const PostDate = styled.h5`
+  margin-right: 20px;
   color: ${({ theme }) => theme.bgText};
 `;
 
